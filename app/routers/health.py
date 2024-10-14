1dcf068f5ecc8fb0d58d6f2e2b407c60b7f18240
sangwon432 <swonl0622@gmail.com>
2024-10-14 07:06:19 -0700

    change #3

--- app/routers/health.py
+++ app/routers/health.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 # test: http localhost:8001/health
 # review this later
+# for some reason, I always have to chmod -x+ before running deploy.
 
 router = APIRouter(prefix = "/health", tags=["Health"])
 
